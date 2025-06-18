16fdcbdd654ff3faa673541730b0b2d9bb34ff2d
Fabrioco <fabriciooliveiralopes50@gmail.com>
2025-06-17 22:17:38 -0300

    feat(footer-component): criei o componente de footer

---
+++ src/components/footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="w-full text-xs text-gray-200 bg-purple-700 text-center px-4 py-2">
+      <p>@2025 ContratoIA - Todos os direitos reservados.</p>
+    </footer>
+  );
+}
