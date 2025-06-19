7acf47c937dd41e7339c9558ae9ac21e676b4c0f
Fabrioco <fabriciooliveiralopes50@gmail.com>
2025-06-19 02:29:02 -0300

    feat(style-contact-page): fiz a estilização final da pagina

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -2,9 +2,9 @@ import FormContact from "@/ui/contact/formContact/page";
 
 export default function ContactPage() {
   return (
-    <main>
-      <h1>Entre em contato</h1>
-      <section>
+    <main className="flex flex-col gap-8 p-4 h-screen w-screen items-center justify-between text-center text-purple-700">
+      <h1 className="text-3xl font-bold">Entre em contato</h1>
+      <section className="flex flex-col gap-4">
         <FormContact />
       </section>
     </main>
