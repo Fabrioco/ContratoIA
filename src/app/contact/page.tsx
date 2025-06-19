e14c997b86c16c3be2458644852e24b42da7601e
Fabrioco <fabriciooliveiralopes50@gmail.com>
2025-06-19 02:15:41 -0300

    feat(contact-page): criei o layout da pagina de contato

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,12 @@
+import FormContact from "@/ui/contact/formContact/page";
+
+export default function ContactPage() {
+  return (
+    <main>
+      <h1>Entre em contato</h1>
+      <section>
+        <FormContact />
+      </section>
+    </main>
+  );
+}
